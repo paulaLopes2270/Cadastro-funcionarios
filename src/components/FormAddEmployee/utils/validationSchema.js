24ee1d0309b20afafbe2d3be02a170869e0008d4
Paula <plnhschltz@gmail.com>
2022-09-05 14:57:31 -0300

    maximo de caracteres para os campos do input

--- src/components/FormAddEmployee/utils/validationSchema.js
+++ src/components/FormAddEmployee/utils/validationSchema.js
@@ -3,8 +3,8 @@ import * as yup from "yup";
 export const validationSchema = yup.object().shape({
   name: yup.string().required("Digite o nome"),
   email: yup.string().email("E-mail inválido").required("Digite o e-mail"),
-  cpf: yup.string().min(11).required("Digite o cpf"),
-  phone: yup.string().min(11).required("Digite o telefone"),
+  cpf: yup.string().max(14,"máximo 11 caracteres").required("Digite o cpf"),
+  phone: yup.string().max(11, "máximo 11 caracteres").required("Digite o telefone"),
   birth_date: yup.date().required("Digite a data de aniversário"),
   created_at: yup.date().required("Digite a data de contratação"),
   salary: yup.number().required("Digite a data de contratação"),
